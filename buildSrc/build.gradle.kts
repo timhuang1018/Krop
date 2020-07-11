142686db6395ac9469b9ec2491fa42112289d1bb
timhuang1018 <t8522192@gmail.com>
2020-07-11 13:02:29 +0800

    update gradle ktx for release

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories{
 }
 
 dependencies{
-    implementation("com.android.tools.build:gradle:3.5.3")
+    implementation("com.android.tools.build:gradle:4.0.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
 
     implementation(gradleApi())
